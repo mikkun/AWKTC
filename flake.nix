bcaa550954af08d21e13b8929cd37742592e6cc9
KUSANAGI Mitsuhisa <mikkun@mbg.nifty.com>
2023-10-06 14:00:01 +0900

    :art: Apply `nixfmt`

--- flake.nix
+++ flake.nix
@@ -5,33 +5,34 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
+    with flake-utils.lib;
+    eachSystem allSystems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        version = "1.2.1";
+      in {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "awktc";
+          inherit version;
+          src = pkgs.fetchFromGitHub {
+            owner = "mikkun";
+            repo = "AWKTC";
+            rev = "v${version}";
+            hash = "sha256-m6CsdtuUtUUtjhzs0rKXp+1A2KQgJtBaWnK2pK8L/rc=";
+          };
 
-  with flake-utils.lib; eachSystem allSystems (system: let 
-    pkgs = import nixpkgs { inherit system; };
-    version = "1.2.1";
-  in  {
-    packages.default = pkgs.stdenv.mkDerivation {
-      pname = "awktc";
-      inherit version;
-      src = pkgs.fetchFromGitHub {
-        owner = "mikkun";
-        repo = "AWKTC";
-        rev = "v${version}";
-        hash = "sha256-m6CsdtuUtUUtjhzs0rKXp+1A2KQgJtBaWnK2pK8L/rc=";
-      };
+          nativeBuildInputs =
+            builtins.attrValues { inherit (pkgs) makeWrapper; };
+          buildInputs = builtins.attrValues { inherit (pkgs) gawk; };
 
-      nativeBuildInputs = builtins.attrValues { inherit (pkgs) makeWrapper; };
-      buildInputs = builtins.attrValues { inherit (pkgs) gawk; };
+          installPhase = ''
+            install -Dm 755 "awktc.awk" "$out/bin/awktc"
+          '';
 
-      installPhase = ''
-        install -Dm 755 "awktc.awk" "$out/bin/awktc"
-      '';
-
-      postFixup = ''
-        wrapProgram "$out/bin/awktc" \
-          --set PATH ${pkgs.lib.makeBinPath [ pkgs.coreutils pkgs.gawk ]};
-      '';
-    };
-  });
+          postFixup = ''
+            wrapProgram "$out/bin/awktc" \
+              --set PATH ${pkgs.lib.makeBinPath [ pkgs.coreutils pkgs.gawk ]};
+          '';
+        };
+      });
 }
-
